9ef88050bcb8389366a8155b83f874cb5989d1f1
caixiaowei <caixiaowei@caixiaoweideMacBook-Pro.local>
2017-11-09 13:52:21 +0800

    20171109

---
+++ src/pages/buyvip/buyvip.ts
@@ -0,0 +1,17 @@
+import { InAppPurchase } from '@ionic-native/in-app-purchase';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'page-buyvip',
+  templateUrl: 'buyvip.html'
+})
+
+export class ListsPage {
+  constructor(private iap: InAppPurchase) {
+
+  }
+
+
+
+}
